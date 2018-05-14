6f2fa6244096471302565e5121432b8b18eb3b1a
Tzu-ping Chung <uranusjr@gmail.com>
2018-05-14 22:45:28 +0800

    Comment setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import find_packages, setup
 
 # Put everything in setup.cfg, except those that don't actually work?
 setup(
+    # These really don't work.
     package_dir={'': 'src'},
     packages=find_packages('src'),
+
+    # I need this to be dynamic.
     version=(
         pathlib.Path(__file__)
         .parent.joinpath('src', 'pent', 'version.txt')
